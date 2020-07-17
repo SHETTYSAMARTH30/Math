582c3965621d7e214cd2e2b04a5897f39cb34054
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-07-16 21:09:38 -0700

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,33 @@
+class SquareRoot {
+    public int mySqrt(int x) {
+        
+        if(x < 2)
+            return x;
+        
+        int left = 2;
+        int right = x/2;
+        int pivot;
+        long num;
+        
+        while(left <= right){
+            
+            pivot = left + (right - left)/2;
+            
+            num = (long)pivot * pivot;
+            
+            if(num > x)
+                right = pivot - 1;
+            
+            else if(num < x)
+                left = pivot + 1;
+            
+            else
+                return pivot;
+                
+        }
+        
+        return right;
+        
+        
+    }
+}
