ee4d2dd48d9dbb4d75081348c65859af210233d0
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-08-02 23:01:57 -0700

    Create ProductSum.java

---
+++ ProductSum.java
@@ -0,0 +1,19 @@
+class ProductSum {
+    public int subtractProductAndSum(int n) {
+        
+        int cal = 0;
+        
+        int product = 1;
+        int sum = 0;
+        
+        while(n > 0){
+            
+            product *= n % 10;
+            sum += n % 10;
+            
+            n = n/10;            
+        }
+        
+        return product - sum;
+    }
+}
